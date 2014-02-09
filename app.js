145c2799493adde9540b6d5faebf7cc8ef6dfff4
Lauren Ashpole <lauren@laurenashpole.com>
2014-02-09 09:57:19 -0500

    With Facebook

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var swig = require('swig');
+var passport = require('passport'),
+FacebookStrategy = require('passport-facebook').Strategy;
 
 var app = express();
 app.engine('html', swig.renderFile);
@@ -23,6 +25,10 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -34,6 +40,53 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.post('/upload', routes.upload);
+app.get('/account', ensureAuthenticated, routes.account);
+app.get('/login', routes.login);
+
+//FB
+
+var FACEBOOK_APP_ID = "249678341873679";
+var FACEBOOK_APP_SECRET = "3689affe76888332a37ee31e8d7e8ffa";
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new FacebookStrategy({
+	clientID: FACEBOOK_APP_ID,
+	clientSecret: FACEBOOK_APP_SECRET,
+	callbackURL: "http://localhost:3000/auth/facebook/callback"
+	},
+	function(accessToken, refreshToken, profile, done) {
+		process.nextTick(function () {
+		console.log("profile:", profile);
+		console.log("ACCESS", accessToken);
+		console.log("REFRESH:", refreshToken),
+		done(null, profile);
+	});
+}));
+
+app.get('/auth/facebook',
+	passport.authenticate('facebook'));
+app.get('/auth/facebook/callback',
+	passport.authenticate('facebook', { successRedirect: '/',
+	failureRedirect: '/login' }));
+
+app.get('/logout', function(req, res){
+	req.logout();
+	res.redirect('/');
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
+
+//
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -9,33 +9,38 @@ var client = require('../client');
 var showError = require('../client');
 
 exports.index = function(req, res){
-  res.render('index');
+  res.render('index', { user: req.user });
+};
+
+exports.account = function(req, res){
+  res.render('account', { user: req.user });
+};
+
+exports.login = function(req, res){
+  res.render('login', { user: req.user });
 };
 
 exports.upload = function(req, res){
-	// console.log(req);
-	console.log(req.files);
-	// var file = req.files.file;
-
 
 	if (typeof req.files.file.path !== 'undefined') {
 		var fileArray = [req.files.file];
-		} else {
-			var fileArray = req.files.file;
-		}
-		fileArray.forEach(function(file){
-			var filePath = file.path;
-			var targetPath = path.resolve('./uploads/', file.name);
+	} else {
+		var fileArray = req.files.file;
+	}
+
+	fileArray.forEach(function(file){
+		var filePath = file.path;
+		var targetPath = path.resolve('uploads/', file.name);
 
 		fs.rename(filePath, targetPath, function(err) {
-            if (err) throw err;
-            console.log("Upload completed!");
-        });
+			if (err) throw err;
+			console.log("Upload completed!");
+		});
 
 		fs.readFile(targetPath, function(error, data) {
-		if (error) {
+			if (error) {
 			return showError;
-			}
+		}
 
 			client.writeFile(targetPath, data, function(error, stat) {
 				if (error) {
@@ -43,16 +48,7 @@ exports.upload = function(req, res){
 				}
 			});
 		});
+	});
 
-
-
-
-	// var file = files.file;
-
-
-
-
-});
-
-  res.redirect('/');
+res.redirect('/');
 };
