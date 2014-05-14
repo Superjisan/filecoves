4cb65e77368a837b4a841608da6b19dec0ba8ca5
Jisan Zaman <jzthegreat@gmail.com>
2014-05-14 11:40:50 -0400

    added selenium first test

--- test/test_selenium.test.js
+++ test/test_selenium.test.js
@@ -1,10 +1,18 @@
-var webdriver = require('selenium-webdriver');
+var assert = require('assert'),
+test = require('selenium-webdriver/testing'),
+webdriver = require('selenium-webdriver');
 
-var driver = new webdriver.Builder().
-   withCapabilities(webdriver.Capabilities.chrome()).
-   build();
-
+test.describe('Google Search', function() {
+  test.it('should work', function() {
+    var driver = new webdriver.Builder().
+    withCapabilities(webdriver.Capabilities.chrome()).
+    build();
 driver.get('http://www.google.com');
-driver.findElement(webdriver.By.name('q')).sendKeys('simple programmer');
-driver.findElement(webdriver.By.name('btnG')).click();
-driver.quit();
+    var searchBox = driver.findElement(webdriver.By.name('q'));
+    searchBox.sendKeys('simple programmer');
+    searchBox.getAttribute('value').then(function(value) {
+      assert.equal(value, 'simple programmer');
+    });
+    driver.quit();
+  });
+});
